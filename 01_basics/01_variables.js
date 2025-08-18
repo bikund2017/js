1f4597ecde1d30619fa9eaea6d15e4cc1ef938d3
bikundkumar <bikund2017@outlook.com>
2025-08-18 17:21:08 +0530

    add 01_basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553;
+let accountEmail = "kumarbikund2017@gmail.com";
+var accountPassword = "12345";
+accountCity = "Noida";
+let accountState;
+
+// accountId = 2
+
+accountEmail = "abc@gmail.com";
+accountPassword = 2323232;
+
+accountCity = "Delhi";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/* 
+    Var variable ko use nahi karna hai 
+*/
