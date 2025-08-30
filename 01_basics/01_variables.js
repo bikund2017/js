09c6bff387de69aa193af606c69d84a4caaea890
bikundkumar <bikund2017@outlook.com>
2025-08-30 17:02:11 +0530

    changes some line of code

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,7 @@ const accountId = 144553;
 let accountEmail = "kumarbikund2017@gmail.com";
 var accountPassword = "12345";
 accountCity = "Noida";
-let accountState;
+let accountState; 
 
 // accountId = 2
 
@@ -10,6 +10,8 @@ accountEmail = "abc@gmail.com";
 accountPassword = 2323232;
 
 accountCity = "Delhi";
+
+accountState = "Punjab"
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
 
