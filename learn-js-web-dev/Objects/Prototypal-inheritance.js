e690f48fb327f1738a6fa0bd2bbeb6704955c2de
bikundkumar <bikund2017@outlook.com>
2025-09-16 16:36:39 +0530

    add Prototypal-inheritance

---
+++ learn-js-web-dev/Objects/Prototypal-inheritance.js
@@ -0,0 +1,32 @@
+// let myObject = {
+//     'booleanValue' : true
+// };
+
+// console.log(myObject);
+
+// let emptyObject = {};
+
+// console.log(emptyObject.__proto__);
+
+// let myObj = {
+//     "value": 5
+// };
+// let protoParent = {
+//     "protoValue" : true
+// }
+// myObj;
+
+// Object.getPrototypeOf(myObj);
+// Object.setPrototypeOf(myObj, protoParent);
+
+let myObj = {
+    "myValue": 100
+}
+Object.hasOwn(myObj, "myValue");
+
+myObj.__proto__;
+
+Object.hasOwn(myObj, '__proto__');
+
+
+
