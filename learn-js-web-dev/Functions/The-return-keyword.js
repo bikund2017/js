fcb2eecaa4ab637c5e885052c5aa20296214da3e
bikundkumar <bikund2017@outlook.com>
2025-09-16 14:43:18 +0530

    the return keyword

---
+++ learn-js-web-dev/Functions/The-return-keyword.js
@@ -0,0 +1,135 @@
+// const myFunction = function () {
+//   return 2 + 2;  // 4
+// };
+// console.log(myFunction());
+
+// const myFunction = function() {
+//     return 2 + 2;
+// }
+
+// console.log(myFunction() + myFunction());        // 8
+
+// const myFunction = function() {
+//     return;
+// }
+
+// console.log(myFunction())    // undefined
+
+// const myFunction = function() {
+//     return true;
+//     console.log("This is a string");
+// }
+
+// console.log(myFunction())
+
+// const myFunction = () => {
+//     return true;
+//     console.log('This is a string')
+// }
+
+// console.log(myFunction())
+
+// (function () {
+//     console.log("This is a string");
+//   return true;
+// })();
+
+// const myArrowFunction = function() {
+//     console.log("This is a string")
+//     return true;
+// }
+
+// myArrowFunction();
+
+// const myRegularFunction = () => {
+//     console.log("This is a string");
+//     return true;
+// }
+
+// myRegularFunction();
+
+// const myFunction = (myParameter) => {
+//     if (myParameter === undefined) {
+//         return "This is the result";
+//     }
+//     return "This is the alternate result.";
+// }
+
+// console.log(myFunction())
+// console.log(myFunction(true))
+
+// const myArrowFunction = ((myParameter) => {
+//     if (myParameter === undefined) {
+//         return "This is the result";
+//     }
+//     return "This is the alternate result.";
+// })
+
+// // console.log(myArrowFunction());
+// // console.log(myArrowFunction(true));
+
+// const myArrowFunction = ((myParameter) => {
+//     if (myParameter === undefined) {
+//         return "This is the result";
+//     }
+//     return "This is the alternate result.";
+// })();   // <- These () at the end invoke the function!
+
+// function myFunction(myString) {
+//     if (typeof myString !== "string") {
+//         return false;
+//     }
+//     if (myString.length >= 5) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(myFunction("bikund"))
+
+// const myFunction = ((myString) => {
+//     if (typeof myString !== "string") {
+//         return false;
+//     }
+//     if (myString.length >= 5) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// })("bikund");
+
+// console.log(myFunction)
+
+// const myFunction = (myString) => {
+//     if (typeof myString !== "string") {
+//         return false;
+//     }
+//     if (myString.length >= 5) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+
+// console.log(myFunction("bikund"));
+
+// const myFunction = (myString) => {
+//     if (typeof myString !== "string") return false;
+//     if (myString.length >= 5) return true;
+//     else return false;
+// }
+
+// console.log(myFunction("bikund"));
+
+// const myFunction = () => 2 + 2;
+
+// console.log(myFunction())
+
+// const myFunction = () => {
+//   return 2 + 2;
+// };
+
+// console.log(myFunction());
