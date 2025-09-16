04eff3ead5a78414355c414677fb6e9651e31f1c
bikundkumar <bikund2017@outlook.com>
2025-09-16 16:35:36 +0530

    add Introduction-to-objects

---
+++ learn-js-web-dev/Objects/Introduction-to-objects.js
@@ -0,0 +1,63 @@
+// {
+//     "myProperty" : true
+// }
+
+// let carAttributes = {
+//     "color" : "red"
+// }
+
+// console.log(carAttributes);
+// console.log(carAttributes.color);
+
+// let myObject = {
+//     'key': {
+//         "subkey" : true,
+//         'othersubKey' : false
+//     }
+// }
+
+// console.log(myObject);
+// console.log(myObject.key);
+// console.log(myObject.key.subkey);
+// console.log(myObject.key.othersubKey);
+
+// const myObject = {
+//     name : "Bikund",
+//     age: 20,
+//     myMethod() {
+//         console.log("This is a method.");
+//     }
+// }
+
+// console.log(myObject);
+// console.log(myObject.myProperty);
+// console.log(myObject.myMethod());
+
+// const myObject = new Object({
+//   name: "Bikund",
+//   age: 20,
+//   myMethod() {
+//     console.log("This is a method")
+//   },
+// });
+
+// console.log(myObject);
+// console.log(myObject.name);
+// console.log(myObject.age);
+// console.log(myObject.myMethod());
+
+// let myObject = new Object();
+
+// myObject.isAdult = true;
+// myObject.city = "Noida";
+
+
+// let myObject = new Object(10);
+// console.log(myObject);
+
+let myObject = new Object( {myValue : 10} );
+
+console.log(myObject);
+
+
+
