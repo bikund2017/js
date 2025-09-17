4647d9b94ff65216fa643b90892431d27e2db47b
bikundkumar <bikund2017@outlook.com>
2025-09-17 18:11:18 +0530

    refactor: Reorganize project folder structure
    - Group conceptual learning folders into a new `learning-path` directory.
    
    - Consolidate projects into a single `projects` directory.
    
    - Remove duplicate `practice` directory.
    
    - Correct typos in folder names for consistency.

--- practice/Digital-Clock/main.js
+++
@@ -1,8 +0,0 @@
-// const clock = document.getElementById("clock");
-const clock = document.querySelector('#clock')
-// console.log(clock);
-
-setInterval(function () {
-  let date = new Date();
-  clock.innerHTML = date.toLocaleTimeString("en-US");
-}, 1000);
